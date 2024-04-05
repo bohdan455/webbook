f47af9499d6275c3ba8ed51beb3997830541d51b
Bohdan <bohdan_voroniak@epam.com>
2024-04-05 15:28:58 +0300

    add header

--- WebBook.Client/src/app/core/components/main-button/main-button.component.ts
+++ WebBook.Client/src/app/core/components/main-button/main-button.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {style} from "@angular/animations";
 
 @Component({
   selector: 'app-main-button',
   templateUrl: './main-button.component.html',
   styleUrl: './main-button.component.scss'
 })
-export class MainButtonComponent {
-
+export class MainButtonComponent{
+  @Input() height: string;
+  @Input() width: string;
+  @Input() fontSize: string
+  @Output() action = new EventEmitter()
+  protected readonly style = style;
 }
