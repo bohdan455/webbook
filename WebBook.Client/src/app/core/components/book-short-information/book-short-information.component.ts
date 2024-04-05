0ce10f67ee46f577afd4f1b8cbbd03c398a2a5ac
Bohdan <bohdan_voroniak@epam.com>
2024-04-05 17:45:25 +0300

    add book short information components

--- WebBook.Client/src/app/core/components/book-short-information/book-short-information.component.ts
+++ WebBook.Client/src/app/core/components/book-short-information/book-short-information.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-book-short-information',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './book-short-information.component.scss'
 })
 export class BookShortInformationComponent {
-
+  @Input() imgSrc: string;
+  @Input() name: string;
+  @Input() author: string;
+  @Input() genre: string;
+  @Input() numberOfPages: string;
 }
