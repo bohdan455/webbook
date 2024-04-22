5ec666edad8f2c8adb77269f90cf46a25e9f6bf4
Bohdan <bohdan_voroniak@epam.com>
2024-04-22 10:57:06 +0300

    add book information header

--- WebBook.Client/src/app/pages/book-information/book-information.component.ts
+++ WebBook.Client/src/app/pages/book-information/book-information.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-book-information',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './book-information.component.scss'
 })
 export class BookInformationComponent {
-
+  imgSrc: string = "/assets/img/book-image.png";
+  name: string = "Пригоди віктора";
+  author: string = "Невідомий автор";
+  numberOfPages: string = "124";
+  stars: string = "124";
+  views: string = "1345";
+  price: string = "Безкоштовно"
+  status: string = "Завершено";
+  genre: string = "Наукова фантастика"
+  annotation: string = "Ця книга - це подорож у світ фантастичних можливостей та нескінченних  ідей. Відкрийте для себе унікальний світ, де межа між реальністю та  уявою розтанула, і кожна сторінка приносить нові відкриття та враження.  Це історія про те, як слова оживають на сторінках, а читачі опиняються у  центрі подій, які можуть змінити все."
 }
